9dfa4093b72b8640e9cb685a37433bb9d7e22ad4
Tamara Katic <tamarakatic@gmail.com>
2018-01-16 21:19:27 +0100

    Add preprocessing functions

--- src/data/preprocessor.py
+++ src/data/preprocessor.py
@@ -1,4 +1,5 @@
 import re
+from nltk.corpus import stopwords
 
 NEGATIVE_CONSTRUCTS = set([
     "ain't",
@@ -19,7 +20,24 @@ NEGATIVE_CONSTRUCTS = set([
     "shouldn't",
     "wasn't"
     "weren't",
-    "won't",
+    "won't"
+])
+
+URLS = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+' \
+    '[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+' \
+    '[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.' \
+    '[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})'
+
+
+POSITIVE_EMOTICONS = set([
+    ':-)', ':)', ':-]', ':-3', ':3', ':^)',
+    '8-)', '8)', '=]', '=)', ':-D', ':D', ':-))'
+])
+
+NEGATIVE_EMOTICONS = set([
+    ':-(', ':(', ':-c', ':c', ':<', ':[', ':''-(',
+    ':-[', ':-||', '>:[', ':{', '>:(', ':-|', ':|',
+    ':/', ':-/', ':\'', '>:/', ':S'
 ])
 
 
@@ -40,3 +58,30 @@ class Preprocessor(object):
     def remove_repeating_vowels(self, sentence):
         """Removes unnecessary repeating vowels. i.e. cooooool -> cool"""
         return re.sub(r'(.)\1+', r'\1\1', sentence)
+
+    def replace_emoticons_with_tags(self, sentence):
+        words = sentence.split()
+
+        for i, word in enumerate(words):
+            if word in POSITIVE_EMOTICONS:
+                words[i] = 'positive'
+            if word in NEGATIVE_EMOTICONS:
+                words[i] = 'negative'
+        return ' '.join(words)
+
+    def remove_whitespace(self, sentence):
+        return re.sub(r'\s+', ' ', sentence)
+
+    def remove_punctuation(self, sentence):
+        return re.sub(r'[^\w\s\']', '', sentence)
+
+    def remove_urls(self, sentence):
+        return re.sub(URLS, '', sentence)
+
+    def remove_emails(self, sentence):
+        return re.sub(r'\S*@\S*\s?', '', sentence)
+
+    def remove_stopwords(self, sentence):
+        stop = set(stopwords.words('english'))
+        words = sentence.lower().split()
+        return ' '.join([word for word in words if word not in stop])
