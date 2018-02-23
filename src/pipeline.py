30f608f38ca541121020ee4a8ee53aa4a89d9e96
Tamara Katic <tamarakatic@gmail.com>
2018-02-24 00:13:34 +0100

    Try glove and tfidf vectorizer

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,11 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from mean_embedding_vectorizer import MeanEmbeddingVectorizer
+from data.glove_dataset import glove_dataset
+from tfidf_embedding_vectorizer import TfidfEmbeddingVectorizer
+
+glove_w2v = glove_dataset('../dataset/glove.840B.300d.txt')
 
 
 def bag_of_words(classifier, tf_idf=True):
@@ -9,5 +14,17 @@ def bag_of_words(classifier, tf_idf=True):
     else:
         steps = [('vect', CountVectorizer())]
 
+    steps.append(('cls', classifier))
+    return Pipeline(steps)
+
+
+def glove_vectorizer(classifier):
+    steps = [('vect', MeanEmbeddingVectorizer(glove_w2v))]
+    steps.append(('cls', classifier))
+    return Pipeline(steps)
+
+
+def tfidf_vectorizer(classifier):
+    steps = [('vect', TfidfEmbeddingVectorizer(glove_w2v))]
     steps.append(('cls', classifier))
     return Pipeline(steps)
--- src/train_model.py
+++ src/train_model.py
@@ -11,39 +11,49 @@ from sklearn.metrics import classification_report
 current_filepath = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = os.path.abspath(os.path.join(current_filepath, os.pardir))
 
-ROWS = 100000
+ROWS = 300000
 
 
 if __name__ == "__main__":
-    data_path = os.path.join(ROOT_PATH, 'dataset/data.csv')
+    data_path = os.path.join(ROOT_PATH, 'dataset/400_data.csv')
     data = load_data(path=data_path, rows=ROWS)
 
     options = Options.all()
     options.remove(Options.STEMMER)
+    options.remove(Options.LEMMATIZER)
     options.remove(Options.SPELLING)
+    options.remove(Options.STOPWORDS)
 
     samples, labels = clean_data(data, options)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        samples, labels, test_size=0.33, random_state=42
+        samples, labels, test_size=0.2, random_state=42
     )
 
-    bow_log_regression = pipeline.bag_of_words(
-        classifier=LogisticRegression(),
-        tf_idf=True
+    # bow_log_regression = pipeline.bag_of_words(
+    #     classifier=LogisticRegression(),
+    #     tf_idf=True
+    # )
+    #
+    # bow_log_regression.set_params(
+    #     vect__ngram_range=(1, 5),
+    #     vect__max_features=500000,
+    #     cls__C=10.0
+    # )
+
+    glove_log_regression = pipeline.glove_vectorizer(
+        classifier=LogisticRegression()
     )
 
-    bow_log_regression.set_params(
-        vect__ngram_range=(1, 5),
-        vect__max_features=500000,
-        cls__C=10.0
-    )
+    # glove_log_regression.set_params(
+    #     cls__C=1.0
+    # )
 
     print("=> Training model...")
-    bow_log_regression.fit(X_train, y_train)
+    glove_log_regression.fit(X_train, y_train)
 
     print("=> Validating model...")
-    predictions = bow_log_regression.predict(X_test)
+    predictions = glove_log_regression.predict(X_test)
 
     report = classification_report(
         y_test,
