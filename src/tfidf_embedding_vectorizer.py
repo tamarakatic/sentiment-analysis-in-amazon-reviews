cd5042184583be23573e5f59d5d7895b17f20f75
Tamara Katic <tamarakatic@gmail.com>
2018-02-24 00:10:27 +0100

    Create tfidf embedding vectorizer class

---
+++ src/tfidf_embedding_vectorizer.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import defaultdict
+
+
+class TfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        if len(word2vec) > 0:
+            self.dim = len(word2vec[next(iter(word2vec))])
+        else:
+            self.dim = 0
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+        return self
+
+    def transform(self, X):
+        return np.array([
+                np.mean([self.word2vec[w] * self.word2weight[w]
+                        for w in words.split() if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+        ])
