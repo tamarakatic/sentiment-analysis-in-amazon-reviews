1dd5736d4e44e2c3cff78a18fc40f7b89a5d8f98
Tamara Katic <tamarakatic@gmail.com>
2018-02-25 19:38:00 +0100

    Add check for dataset

--- src/data/glove_dataset.py
+++ src/data/glove_dataset.py
@@ -6,8 +6,9 @@ def glove_dataset(path):
     glove_w2v = {}
     with open(path, 'rb') as f:
         for line in f:
-            values = line.split()
-            word = values[0].decode("utf-8")
-            coefs = np.asarray(values[1:], dtype='float32')
-            glove_w2v[word] = coefs
+            if line != 0:
+                values = line.split()
+                word = values[0].decode("utf-8")
+                coefs = np.asarray(values[1:], dtype='float32')
+                glove_w2v[word] = coefs
     return glove_w2v
