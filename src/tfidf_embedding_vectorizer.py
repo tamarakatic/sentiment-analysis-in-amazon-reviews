39568529e0f415067ac39ac673d56ddc62d3608c
Tamara Katic <tamarakatic@gmail.com>
2018-02-25 18:11:31 +0100

    Change name of variables

--- src/tfidf_embedding_vectorizer.py
+++ src/tfidf_embedding_vectorizer.py
@@ -4,27 +4,27 @@ from collections import defaultdict
 
 
 class TfidfEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        self.word2weight = None
-        if len(word2vec) > 0:
-            self.dim = len(word2vec[next(iter(word2vec))])
+    def __init__(self, embedding):
+        self.embedding = embedding
+        self.tfidf_weights = None
+        if len(embedding) > 0:
+            self.dim = len(embedding[next(iter(embedding))])
         else:
             self.dim = 0
 
     def fit(self, X, y):
-        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf = TfidfVectorizer()
         tfidf.fit(X)
         max_idf = max(tfidf.idf_)
-        self.word2weight = defaultdict(
+        self.tfidf_weights = defaultdict(
             lambda: max_idf,
             [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
         return self
 
     def transform(self, X):
         return np.array([
-                np.mean([self.word2vec[w] * self.word2weight[w]
-                        for w in words.split() if w in self.word2vec] or
+                np.mean([self.embedding[w] * self.tfidf_weights[w]
+                        for w in words.split() if w in self.embedding] or
                         [np.zeros(self.dim)], axis=0)
                 for words in X
         ])
