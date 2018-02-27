de19289121627fce235ead9d1cd17347f03034be
Tamara Katic <tamarakatic@gmail.com>
2018-02-28 00:03:24 +0100

    Add more classifier and google news word2vec

--- src/train_model.py
+++ src/train_model.py
@@ -28,10 +28,9 @@ if __name__ == "__main__":
 
     options = Options.all()
     options.remove(Options.STEMMER)
-    # options.remove(Options.LEMMATIZER)
+    options.remove(Options.LEMMATIZER)
     options.remove(Options.SPELLING)
     options.remove(Options.STOPWORDS)
-    # options.remove(Options.NEGATIVE_CONSTRUCTS)
 
     samples, labels = clean_data(data, options)
 
@@ -39,45 +38,45 @@ if __name__ == "__main__":
         samples, labels, test_size=0.2, random_state=42
     )
 
-    # bow_log_regression_tfidf = pipeline.bag_of_words(
-    #     classifier=LogisticRegression(),
-    #     tf_idf=True
-    # )
-    #
-    # bow_linear_svc_tfidf = pipeline.bag_of_words(
-    #     classifier=LinearSVC(kernel='linear'),
-    #     tf_idf=True
-    # )
-    #
-    # bow_svc_tfidf.set_params(
-    #     vect__ngram_range=(1, 5),
-    #     vect__max_features=500000,
-    #     cls__C=10.0
-    # )
+    bow_log_regression_tfidf = pipeline.bag_of_words(
+        classifier=LogisticRegression(),
+        tf_idf=True
+    )
 
-    # bow_multi_nb = pipeline.glove_mean_vectorizer(
-    #     classifier=MultinomialNB()
-    # )
-    #
-    # bow_multi_nb.set_params(
-    #     vect__ngram_range=(1, 5),
-    #     vect__max_features=500000
-    # )
+    bow_linear_svc_tfidf = pipeline.bag_of_words(
+        classifier=LinearSVC(kernel='linear'),
+        tf_idf=True
+    )
 
-    # bow_bernulli_nb_tfidf = pipeline.bag_of_words(
-    #     classifier=BernoulliNB(),
-    #     tf_idf=True
-    # )
-    #
-    # bow_bernulli_nb_tfidf.set_params(
-    #     vect__ngram_range=(1, 5),
-    #     vect__max_features=500000
-    # )
-    # w2v_model = Word2Vec(np.hstack((X_train, X_test)),
-    #                      size=100,
-    #                      window=5,
-    #                      min_count=WORKERS)
-    # w2v = {w: vec for w, vec in zip(w2v_model.wv.index2word, w2v_model.wv.syn0)}
+    bow_linear_svc_tfidf.set_params(
+        vect__ngram_range=(1, 5),
+        vect__max_features=500000,
+        cls__C=10.0
+    )
+
+    bow_multi_nb = pipeline.glove_mean_vectorizer(
+        classifier=MultinomialNB()
+    )
+
+    bow_multi_nb.set_params(
+        vect__ngram_range=(1, 5),
+        vect__max_features=500000
+    )
+
+    bow_bernulli_nb_tfidf = pipeline.bag_of_words(
+        classifier=BernoulliNB(),
+        tf_idf=True
+    )
+
+    bow_bernulli_nb_tfidf.set_params(
+        vect__ngram_range=(1, 5),
+        vect__max_features=500000
+    )
+    w2v_model = Word2Vec(np.hstack((X_train, X_test)),
+                         size=100,
+                         window=5,
+                         min_count=WORKERS)
+    w2v = {w: vec for w, vec in zip(w2v_model.wv.index2word, w2v_model.wv.syn0)}
 
     glove_mean_w2v_logistic_reg = pipeline.glove_mean_vectorizer(
         classifier=LogisticRegression(),
